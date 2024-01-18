ab585270bfb72fb59a8f2181bf0621831c0f9323
Enrico Cirignaco <enrico.cirignaco@students.bfh.ch>
2024-01-18 17:01:19 +0100

    script variables replaced by env variables

--- video_converter/script.sh
+++ video_converter/script.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-bin=./bin
-media_pool=./media
-converted_tag=_converted # must also be manually changed in convert_command
-end_hour=14
 
+# create a bin folder if it doesn't exist
+mkdir -p $BIN
 # put all non video files recursively in the bin folder
-find $media_pool -type f \
+find $MEDIA_POOL -type f \
   -exec bash -c 'file -b --mime-type "$0" | grep -qvE "^video/"' {} \; \
-  -exec mv -t $bin {} +
+  -exec mv -t $BIN {} +
 
 #remove all empty folders
-find $media_pool -type d -empty -delete
+find $MEDIA_POOL -type d -empty -delete
 
 # Convert all founded files. after end_hour, finish current conversion and exit
 # Iterate over each file found by 'find' command
@@ -19,13 +17,13 @@ while IFS= read -r -d '' file; do
     current_hour=$(date +%H)
     
     # If the current time is beyond the end hour, exit the loop
-    if [ "$current_hour" -ge "$end_hour" ]; then
-        echo "Stopping script as it's past $end_hour AM."
+    if [ "$current_hour" -ge "$END_TIME" ]; then
+        echo "Stopping script as it's past $END_TIME:00."
         break
     fi
 
-    # Convert the file
+    #Convert the file
     input="$file"
-    output="${input%.*}_${converted_tag}.mp4"
-    ffmpeg -loglevel warning -report -hide_banner -i "$input" -c:v libx264 -crf 23 -c:a aac -strict experimental -b:a 192k "$output" && rm "$input"
-done < <(find "$media_pool" -type f -not -iname "*${converted_tag}*" -print0)
+    output="${input%.*}_${CONVERTED_TAG}.mp4"
+    ffmpeg -loglevel warning -hide_banner -i "$input" -c:v libx264 -crf 23 -c:a aac -strict experimental -b:a 192k "$output" && rm "$input"
+done < <(find "$MEDIA_POOL" -type f -not -iname "*${CONVERTED_TAG}*" -print0)
